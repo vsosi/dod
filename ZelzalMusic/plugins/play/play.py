fe81f1242446fa5fe2aac1a57abbb6f69c509fd7
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-08-13 16:45:08 +0300

    Update play.py

--- ZelzalMusic/plugins/play/play.py
+++ ZelzalMusic/plugins/play/play.py
@@ -30,7 +30,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(   
-              text=f"Not DoDy .", url=f"t.me/mmmsc",)                        
+              text=f"Not DoDy .", url=f"t.me/cdody",)                        
         ],        
     ]
 )
@@ -38,10 +38,10 @@ async def check_is_joined(message):
     try:
         userid = message.from_user.id
         user_name = message.from_user.first_name
-        status = await app.get_chat_member("mmmsc", userid)
+        status = await app.get_chat_member("cdody", userid)
         return True
     except Exception:
-        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @mmmsc 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
+        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @cdody 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
         return False
 
 
